8da6481a392414110e024ddaf9291483675b0122
Dief Bell <diefenbaker.bell@gmail.com>
2026-02-12 15:52:50 +0000

    Caching symbols

--- package/src/Sym.ts
+++ package/src/Sym.ts
@@ -1,8 +1,13 @@
 import { Expr } from "./Expr";
 
 export class Sym extends Expr {
-	constructor(public name: string) {
+	static readonly #cache = new Map<string, Sym>();
+
+	constructor(public readonly name: string) {
+		const cached = Sym.#cache.get(name);
+		if (cached) return cached;
 		super();
+		Sym.#cache.set(name, this);
 	}
 
 	toString() {
